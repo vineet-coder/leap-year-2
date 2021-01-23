7eddd9301443039eae958bd57473cab67ce92cc9
vineet <vineetsinghal2312@gmail.com>
2021-01-23 13:28:40 +0530

    fix

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-var btnTranslate = document.querySelector("#btntranslate");
-var textInput = document.querySelector("#txtinput");
+var btnTranslate = document.querySelector("#btn-Translate");
+var txtInput = document.querySelector("#txt-Input");
 var outputDiv = document.querySelector("#output");
 
 function clickHandler() {
-  var year = textInput.value;
+  var num = txtInput.value;
 
-  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-    outputDiv.innerText = year + " is a leap year!!";
-  } else {
-    outputDiv.innerText = year + " is not a leap year!!";
-  }
+  var result = math.pow(num, 3);
+  outputDiv.innerText = result;
 }
 
 btnTranslate.addEventListener("click", clickHandler);
